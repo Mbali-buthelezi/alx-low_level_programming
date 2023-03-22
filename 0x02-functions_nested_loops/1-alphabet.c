589890c6022915a399f3c3b78201ae5daa5c10c5
Mbali Buthelezi <shellzshenge79@gmail.com>
2023-03-22 13:31:06 -0700

    M

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabets in lower case.
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++;)
+		_putchar(i);
+	_putchar('\n');
+}
